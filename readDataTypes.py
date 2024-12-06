5d801240823db59b1051b4a2a5ba6db73c6a2083
sbubby-1 <michaelyao3309@gmail.com>
2024-12-06 14:36:16 -0600

    Initial commit

---
+++ readDataTypes.py
@@ -0,0 +1,39 @@
+import struct
+
+
+def readString(file):
+    length = 0
+    shift = 0
+    while True:
+        byte = file.read(1)[0]
+        length |= (byte & 0x7F) << shift
+        if (byte & 0x80) == 0:
+            break
+        shift += 7
+    data = file.read(length)
+    return data.decode("utf-8")
+
+
+def readInt(file):
+    data = file.read(4)
+    return struct.unpack("<I", data)[0]
+
+
+def readInt64(file):
+    data = file.read(8)
+    return struct.unpack("<Q", data)[0]
+
+
+def readShort(file):
+    data = file.read(2)
+    return struct.unpack("<H", data)[0]
+
+
+def readByte(file):
+    data = file.read(1)
+    return struct.unpack("<B", data)[0]
+
+
+def readBoolean(file):
+    byte = readByte(file)
+    return byte != 0
