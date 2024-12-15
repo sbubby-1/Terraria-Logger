58a6d69d0e07c46e8c5ba8a7b4f823157bee504d
sbubby3309 <168878811+sbubby3309@users.noreply.github.com>
2024-12-15 01:29:17 -0600

    Add docs for WorldInfo.py

--- WorldInfo.py
+++ WorldInfo.py
@@ -1,13 +1,29 @@
+"""
+Contains relevant info regarding an analyzed seed.
+"""
+
 from CustomExceptions.InitializationFailed import InitializationFailed
 
+# Relevant info from the ``Header`` section of the world file. Fields are
+# tagged as relevant in ``HeaderValues.json``.
 relevantInfo = {}
+
+# An array of ``PyramidItem``s.
 pyramidItems = []
 
+# Whether housing has been built in this world at all.
 housingBuilt = False
+
+# Whether the player has talked to the Demolitionist.
 talkedToDemo = False
+
+# Whether the Guide has been housed in the Underworld.
 guideInHell = False
+
+# Whether there is an NPC housed at shimmer.
 npcAtShimmer = False
 
+# Tracks which bosses have been killed.
 bossesSlain = {
     "Deerclops": False,
     "Duke Fishron": False,
@@ -30,6 +46,10 @@ bossesSlain = {
 
 
 def clearWorldInfo():
+    """
+    Resets WorldInfo to its original state.
+    """
+
     for key in bossesSlain:
         bossesSlain[key] = False
 
@@ -50,6 +70,14 @@ def clearWorldInfo():
 
 
 def getSeedInfo():
+    """
+    Returns info that will be saved to a JSON.
+
+    Returns: Dict
+        A Dictionary holding relevant info with values formatted in the style
+        they'll appear in the saved JSON.
+    """
+
     seedInfo = {}
 
     try:
--- readDataTypes.py
+++ readDataTypes.py
@@ -1,15 +1,28 @@
+"""
+Contains functions for parsing and returning the values of various data types.
+"""
+
 import struct
 
 
 def readString(file):
+    # Length of the decoded String
     length = 0
+
+    # Position of the current bit (in multiples of 7)
     shift = 0
     while True:
+        # Read the first byte, extracts the lower 7 bits and shifts them into
+        # position, combines them with length.
         byte = file.read(1)[0]
         length |= (byte & 0x7F) << shift
+
+        # If this is the 8th bit of the byte, it is the last bit of the length.
         if (byte & 0x80) == 0:
             break
+
         shift += 7
+
     data = file.read(length)
     return data.decode("utf-8")
 
