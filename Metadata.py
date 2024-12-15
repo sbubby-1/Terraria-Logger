88867df7ccb3e3d599299139f48404ad201e6968
sbubby3309 <168878811+sbubby3309@users.noreply.github.com>
2024-12-15 01:03:14 -0600

    Add docs for Metadata.py

--- Metadata.py
+++ Metadata.py
@@ -1,28 +1,46 @@
-import json
+"""
+Contains logic for various metadata pertaining to world file reading.
+"""
+
 from collections import OrderedDict
 from enum import Enum
+import json
 
 headerFields = OrderedDict()
 
 HEADER_VALUES_FILEPATH = "HeaderValues.json"
-
-PYRAMID_ITEM_IDS = {848, 857, 866, 934}
-
 MULTIPLIER_FIELDS = {
     "Number of Players",
     "Kill Count Length",
     "Number of Partiers",
     "Number of Tree Tops",
 }
+PYRAMID_ITEM_IDS = {848, 857, 866, 934}
 
 
 def initializeHeaderFields():
+    """
+    Reads in HeaderValues.json, which lists the field name, relevance and data
+    type for each field in the header section of a world file.
+    """
+
     with open(HEADER_VALUES_FILEPATH, "r") as file:
         global headerFields
         headerFields = json.load(file, object_pairs_hook=OrderedDict)
 
 
 def sizeOf(dataType):
+    """
+    Returns the byte size of the provided data type.
+
+    Parameters:
+    ----------
+    dataType: String
+        The data type in String format. A space-separated multiplier can follow
+        the data type.
+    ----------
+    """
+
     match dataType:
         case "Byte":
             return 1
@@ -40,12 +58,17 @@ def sizeOf(dataType):
             return 8
         case "Rect":
             return 16
+        # This case is for "dataType multiplier".
         case _:
             dataType, multiplier = dataType.split(" ")
             return int(multiplier) * sizeOf(dataType)
 
 
 class PyramidItem:
+    """
+    A class that holds relevant info for a pyramid item.
+    """
+
     def __init__(self, itemName, chestX, chestY):
         self.itemName = itemName
         self.chestX = chestX
@@ -53,6 +76,11 @@ class PyramidItem:
 
 
 class OffsetIndices(Enum):
+    """
+    An Enumerable that holds the corresponding index for each section of the
+    world file in the ``offsets`` array.
+    """
+
     INITIALIZATION = -1
     HEADER = 0
     TILE_DATA = 1
