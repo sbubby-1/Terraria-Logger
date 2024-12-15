a04df383847ae62b436147629d867e2e7325d834
sbubby3309 <168878811+sbubby3309@users.noreply.github.com>
2024-12-15 00:48:13 -0600

    Add docs for Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
-from KeyboardListener import startKeyboardListener
 from Display import startDisplay
+from KeyboardListener import startKeyboardListener
 import threading
 
+# The keyboard listener is started on a separate thread to prevent
+# keyboard.wait() from blocking execution of startDisplay().
 listenerThread = threading.Thread(target=startKeyboardListener, daemon=True)
 listenerThread.start()
+
 startDisplay()
