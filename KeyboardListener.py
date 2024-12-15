cd0a0f5ca0a7280937b7c8e2e2f02eaff5f16305
sbubby3309 <168878811+sbubby3309@users.noreply.github.com>
2024-12-15 00:39:45 -0600

    Add docs for KeyboardListener.py

--- KeyboardListener.py
+++ KeyboardListener.py
@@ -1,10 +1,20 @@
-from LoggingLogic import resetTriggered, saveSeed
 from Display import toggleVisibility
+from LoggingLogic import resetTriggered, saveSeed
 import keyboard
 
 
 def startKeyboardListener():
+    """
+    Assigns key combinations to actions and begins keyboard listening.
+    """
+
+    # Analyzes and deletes the first .wld found in the Worlds folder.
     keyboard.add_hotkey("ctrl+shift+m", resetTriggered)
+
+    # Hides or unhides the display window.
     keyboard.add_hotkey("ctrl+shift+h", toggleVisibility)
+
+    # Saves the current seed alongside its relevant info in JSON format.
     keyboard.add_hotkey("ctrl+shift+p", saveSeed)
+
     keyboard.wait()
